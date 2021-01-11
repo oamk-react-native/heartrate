2dbe8d644bc1095cb0ca6151b3f6b1616bf342ff
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-01-11 20:52:00 +0200

    Finished example.

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [lower,setLower] = useState(0);
+  const [upper,setUpper] = useState(0);
+
+  function calculate(text) {
+    setAge(text);
+    setLower((220-text) * 0.65);
+    setUpper((220-text) * 0.85);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Age</Text>
+      <TextInput value={age} onChangeText={text => calculate(text)}></TextInput>
+      <Text>Hr limits</Text>
+      <Text>{lower.toFixed(0)} - {upper.toFixed(0)}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+    marginLeft: 10,
   },
 });
